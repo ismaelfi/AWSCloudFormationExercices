a9ef8cc48dcfddcfa6bb1b9a2cf4000126119f4c
ismailfi <ifinnaoui@gmail.com>
2021-04-02 16:19:27 +0200

    route tables and association with subnets

--- updatestack.sh
+++ updatestack.sh
@@ -1,1 +1,5 @@
-aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+#this sh script gonna update the stack created in the default region
+# You can specify region by adding --region=us-west-2
+
+
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
